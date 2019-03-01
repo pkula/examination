8e1926bae880b49c8261d9f8f43e09e960680e46
pkula <pxkula@gmail.com>
2019-03-01 23:24:08 +0100

    Add models
    create ExamSheet, Answer and AnswerForm models

--- examinations/examSheetsApi/models.py
+++ examinations/examSheetsApi/models.py
@@ -2,16 +2,23 @@ from django.db import models
 
 class Question(models.Model):
     question = models.TextField(max_length=100, default="it's not allowed")
-    max_mark = models.IntegerField()
+    max_mark = models.IntegerField(blank=True)
+    # many to one exam Sheet
 
 class Answer(models.Model):
     answer = models.TextField(max_length=110)
-    mark = models.IntegerField()
+    mark = models.IntegerField() #superuser mark this field
+    #many to one AnswerForm
 
 class ExamSheet(models.Model):
-    questions = wiele questiionss
+    is_published = models.BooleanField(default=False)
+    #questions = wiele questiionss
+    # many to one superuser
 
 class AnswerForm(models.Model):
-    exam_sheet = foreign key   ExamSheet  # one to many
-    answers = //
-    total_mark =   # superuser mark this answers
+    pass
+    #exam_sheet = foreign key   ExamSheet  # one to many
+    #answers = //
+    #total_mark   - its not a field there is a function
+    # many to one user not creator
+    # many to one superuser
